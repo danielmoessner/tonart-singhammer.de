3115f24946c7c6b96ebf96d5a5c4d69e01aabef3
danielmoessner <github@moesibert.de>
2021-06-28 15:55:54 +0200

    navigation mobil

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -39,19 +39,19 @@ function Component() {
         {({ open }) => (
           <>
             <Container layout="sm">
-              <div className="py-6 flex justify-between items-center md:justify-start md:space-x-10">
+              <div className="py-6 flex justify-between items-center lg:justify-start lg:space-x-10">
                 <div className="flex justify-start lg:w-0 lg:flex-1">
                   <Link className="block" to="/">
                     <Logo />
                   </Link>
                 </div>
-                <div className="-mr-2 -my-2 md:hidden">
+                <div className="-mr-2 -my-2 lg:hidden">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none">
                     <span className="sr-only">Menü öffnen</span>
                     <MenuIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
-                <Popover.Group className="hidden md:flex space-x-10">
+                <Popover.Group className="hidden lg:flex space-x-10">
                   {navigation.map((item) => (
                     <Link
                       key={item.name}
@@ -78,7 +78,7 @@ function Component() {
               <Popover.Panel
                 focus
                 static
-                className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+                className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right lg:hidden"
               >
                 <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                   <div className="pt-5 pb-6 px-5">
