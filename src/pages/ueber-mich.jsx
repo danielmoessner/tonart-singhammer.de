3c572c3787e00b2ff690c91155669fe6f9f1bed9
danielmoessner <github@moesibert.de>
2021-06-28 12:40:23 +0200

    about seite

--- src/pages/ueber-mich.jsx
+++ src/pages/ueber-mich.jsx
@@ -87,45 +87,11 @@ function Page({ data }) {
                 <div className="text-base max-w-prose mx-auto lg:max-w-none">
                   <Post color="">{page.header.text}</Post>
                 </div>
-                <div className="mt-5 prose prose-indigo text-gray-0500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
-                  <p>
-                    Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum.
-                    Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra donec ut volutpat donec
-                    laoreet quam urna.
-                  </p>
-                  <p>
-                    Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit nisi lorem
-                    pulvinar. Urna, laoreet fusce nibh leo. Dictum et et et sit. Faucibus sed non
-                    gravida lectus dignissim imperdiet a.
-                  </p>
-                  <p>
-                    Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus,
-                    egestas netus cursus. In.
-                  </p>
-                  <ul>
-                    <li>Quis elit egestas venenatis mattis dignissim.</li>
-                    <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-                    <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-                  </ul>
-                  <p>
-                    Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel
-                    ultricies. Vulputate aliquet velit faucibus semper. Pellentesque in venenatis
-                    vestibulum consectetur nibh id. In id ut tempus egestas. Enim sit aliquam nec,
-                    a. Morbi enim fermentum lacus in. Viverra.
-                  </p>
-                  <h3>How we helped</h3>
-                  <p>
-                    Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium
-                    in volutpat, diam. Montes, magna cursus nulla feugiat dignissim id lobortis
-                    amet. Laoreet sem est phasellus eu proin massa, lectus. Diam rutrum posuere
-                    donec ultricies non morbi. Mi a platea auctor mi.
-                  </p>
-                  <p>
-                    Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed
-                    consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu
-                    morbi.
-                  </p>
-                </div>
+                <div
+                  className="mt-5 prose prose-indigo text-gray-0500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1"
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{ __html: page.html }}
+                />
               </div>
             </div>
           </div>
@@ -146,23 +112,11 @@ export default Page;
 
 export const query = graphql`
   {
-    pagesYaml(slug: { eq: "about" }) {
-      ...meta
-      ...header
-    }
-
     markdownRemark(frontmatter: { slug: { eq: "about" } }) {
       html
       frontmatter {
         ...metaMarkdown
         ...headerMarkdown
-        content {
-          image {
-            childImageSharp {
-              gatsbyImageData
-            }
-          }
-        }
       }
     }
   }
